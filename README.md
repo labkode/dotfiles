4407f166e35b367464c4f2080556681f3db30e70
Hugo Gonzalez <github@hugo.labkode.com>
2024-09-09 19:05:49 +0200

    update

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
 # My dot files
 
-.vimrc generated from: https://vim-bootstrap.com/
-.tmux.conf my own
-.zshrc genereated from oh-my-zsh
-
-
-## vim
-vim .
-:PlugInstall
+- .vimrc
+- .tmux.conf
+- .alacritty.toml
--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,8 @@
 
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/tmux.conf
+
+
+git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
+git clone https://github.com/wsdjeg/vim-fetch ~/.vim/pack/plugins/start/vim-fetch
+
